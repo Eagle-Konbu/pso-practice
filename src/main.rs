93cd669b448f2fe82a49f319baaa61c20c232003
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-05-16 23:28:44 +0900

    Added graph for main.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,35 @@ use pso::pso::Pso;
 
 fn main() {
     println!("Hello, world!");
+
+    let n = 100;
+    let complete_graph = {
+        let mut graph = Vec::new();
+        for i in 0..n {
+            let mut row = Vec::new();
+            for j in 0..n {
+                if i != j {
+                    row.push(j);
+                }
+            }
+            graph.push(row);
+        }
+        graph
+    };
     let mut rng = rand::thread_rng();
-    let mut pso = Pso::new(0.5, 0.5, 0.5, 0.0, 10.0, 100, 5, f, &mut rng);
+    let mut pso = Pso::new(
+        0.5,
+        0.5,
+        0.5,
+        0.0,
+        10.0,
+        100,
+        5,
+        f,
+        &complete_graph,
+        1,
+        &mut rng,
+    );
     pso.run(&mut rng, 100);
 
     println!(
