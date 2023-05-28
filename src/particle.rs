bc8fc2dd6f70e342b64a074ddc022be0082a2454
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-05-28 20:47:08 +0900

    Added distance for particle.

--- src/particle.rs
+++ src/particle.rs
@@ -4,3 +4,13 @@ pub struct Particle {
     pub velocity: Vec<f64>,
     pub personal_best: Vec<f64>,
 }
+
+impl Particle {
+    pub fn distance(&self, other: &Particle) -> f64 {
+        let mut sum = 0.0;
+        for i in 0..self.position.len() {
+            sum += (self.position[i] - other.position[i]).powi(2);
+        }
+        sum.sqrt()
+    }
+}
